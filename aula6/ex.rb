2fe7cc771c71c9898f752c70e03d387791c7c7d9
CaioBrasetti <113940112+CaioBrasetti@users.noreply.github.com>
2023-01-23 10:39:47 -0300

    Adicionando exercícios praticados

---
+++ aula6/ex.rb
@@ -0,0 +1,27 @@
+class Esportista
+  def competir
+    puts "Participando de uma competição"
+  end
+end
+
+class JogadorDeFutebol < Esportista
+  def correr
+    puts "Correndo atrás da bola"
+  end
+end
+
+class Maratonista < Esportista
+  def correr
+    puts "Percorrendo o circuito"
+  end
+end
+
+esportista = Esportista.new
+jogador_de_futebol = JogadorDeFutebol.new
+maratonista = Maratonista.new
+
+jogador_de_futebol.competir
+jogador_de_futebol.correr
+puts
+maratonista.competir
+maratonista.correr
